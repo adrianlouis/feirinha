dcf866a95d5e8427b3dd2a85d67c6ec8bad1b8a1
AdrianLouis <adrian.louis.003@live.com>
2022-02-17 16:10:13 -0300

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function indexMenu(){
+    document.querySelector('.headerMenu').style.opacity=0;
+    document.querySelector('.menuModal').style.left=0;
+}   
+function indexMenuClose(){
+    document.querySelector('.headerMenu').style.opacity=1;
+    document.querySelector('.menuModal').style.left='-101vh';
+
+
+}
