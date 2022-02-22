d927b508aebe017592746352fa8b3fd6ca814fdb
AdrianLouis <adrian.louis.003@live.com>
2022-02-22 00:03:12 -0300

    Calculos aplicados na pagina Cart.html

--- js/cart.js
+++ js/cart.js
@@ -1,24 +1,28 @@
 const container = document.querySelector('.mainContent')
 let id = 1
-var precoTotalCru = 0
 var precoUnitario = 0
+var precoFinalTotal = 0
 
-async function getCart(){
-    const response = await fetch('http://apiorganica.azurewebsites.net/Carrinho/'+id)
+async function getCart() {
+    const response = await fetch('http://apiorganica.azurewebsites.net/Carrinho/' + id)
     const jsonBody = await response.json()
     const container = document.querySelector('.mainContent')
     let template = ''
 
-    document.querySelector('.cartTotalPrice').innerHTML ='Total: '+ jsonBody.valorSubtotal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
     precoTotalCru = jsonBody.valorSubtotal
 
     Promise.all(jsonBody.itens.map(item => {
+        precoFinalTotal += (item.produto.valor * item.quantidade)
+        document.querySelector('.cartTotalPrice').innerHTML = 'Total: R$ ' + precoFinalTotal.toFixed(2)
+        console.log(precoFinalTotal)
         let idImagem = item.imagens[0].codigo
         idImagem = idImagem.toString().padStart(4, 0)
         let extensao = item.imagens[0].extensao
+
         let itemQuantidade = item.quantidade
-        precoUnitario = item.produto.valor 
-        let valorBRL = item.produto.valor .toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+        precoUnitario = item.produto.valor
+
+        let valorBRL = item.produto.valor.toFixed(2)
 
         template = ` <div class="cardCartItem">
 
@@ -27,11 +31,11 @@ async function getCart(){
         <div class="cardEscritas">
 
             <span>${item.produto.titulo}</span>
-            <span class="cartPriceItem">${valorBRL}</span>
+            <span class="cartPriceItem">R$ ${valorBRL}</span>
             <div class="detQtdBtn">
-                <span onclick="addQtd(this)"><i class="fa-light fa-plus" ></i></span>
+                <span onclick="addQtd(this, ${valorBRL})"><i class="fa-light fa-plus" ></i></span>
                 <span id="detNumQtd">${itemQuantidade}</span>
-                <span onclick="lessQtd(this)"><i class="fa-light fa-minus"></i></span>
+                <span onclick="lessQtd(this, ${valorBRL})"><i class="fa-light fa-minus"></i></span>
             </div>
 
         </div>
@@ -39,32 +43,47 @@ async function getCart(){
         container.innerHTML += template
     }))
 }
-function addQtd(el) {
-    let qtd = el.nextElementSibling.innerHTML
-    let precoTotal = document.querySelector('.cartTotalPrice').innerHTML
 
+function addQtd(el, price) {
+    let qtd = el.nextElementSibling.innerHTML
     qtd = parseInt(qtd)
     qtd++
+    precoFinalTotal += price
+    document.querySelector('.cartTotalPrice').innerHTML = "Total: " + precoFinalTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
     el.nextElementSibling.innerHTML = qtd
-    console.log(precoTotalCru )
+    let total = parseFloat(qtd) * price
+    total = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    el.parentNode.previousElementSibling.innerHTML = total
+}
+function lessQtd(el, price) {
+    let qtd = el.previousElementSibling.innerHTML
+    qtd = parseInt(qtd)
+
+    if (qtd == 0) {
+        return
+    } else {
+        qtd--
+        el.previousElementSibling.innerHTML = qtd
+        let total = parseFloat(qtd) * price
+        total = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        el.parentNode.previousElementSibling.innerHTML = total
+        precoFinalTotal -= price
+        document.querySelector('.cartTotalPrice').innerHTML = "Total: " + precoFinalTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    }
 }
 
-async function fetchImg(idImagem){
-    const response = await fetch('https://apiorganica.azurewebsites.net/imagens/'+idImagem)
+function precoFinal() {
+    let precos = document.querySelectorAll('.cartPriceItem')
+    let thisprice = 0
+    precos.forEach(el => {
+        console.log(thisprice + parseInt(el.innerHTML))
+    })
+}
+
+async function fetchImg(idImagem) {
+    const response = await fetch('https://apiorganica.azurewebsites.net/imagens/' + idImagem)
     const jsonBd = await response.json()
     return jsonBd
 }
 
 getCart()
-
-
-function lessQtd(el) {
-    let qtd = el.previousElementSibling.innerHTML 
-    qtd = parseInt(qtd)
-    if(qtd == 0){
-        return
-    }else{
-        qtd--
-        el.previousElementSibling.innerHTML = qtd
-    }
-}
